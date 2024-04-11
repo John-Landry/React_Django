be81e7021411906b0d012f14756eedfade098fdc
John-Landry <johnlandry33@gmail.com>
2024-04-11 10:22:30 -0400

    x

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,14 @@
+{/* 
+  This is where we put navigation between 
+  the pages using react router dom
+
+  This is the route of our application, 
+  and we want to be able to navigate between 
+  different pages by going to a different address in the URL bar
+*/}  
+
 import react from 'react'
+import {B}
 
 function App() {
   return (
